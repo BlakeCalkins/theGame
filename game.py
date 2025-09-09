aae724877aa3e0672a1d6427b225499c95ba2321
Blake <blakeedwincalkins@gmail.com>
2025-09-09 15:47:01 -0600

    add test_one_hundred_thousand_times method

--- game.py
+++ game.py
@@ -1,12 +1,19 @@
 import random
 
 def rogue():
-    return random.randint(1, 4) + random.randint(1, 4) + random.randint(1, 4)
+    return random.randint(1, 4) + random.randint(1, 4) + random.randint(1,4)
 def warrior():
     return random.randint(1, 8) + 1
+def dave_from_accounting():
+    return 5
 
-def test_ten_thousand_times(which_class):
-    pass
+def test_one_hundred_thousand_times(which_class):
+    sum = 0
+    for i in range(100000):
+        sum += which_class()
+    return sum / 100000
 
 def main(): 
-    pass
+    print(test_one_hundred_thousand_times(dave_from_accounting))
+
+main()
