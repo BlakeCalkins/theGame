37e75880a6651a9b9f2bfb590dc37c5f3b60af11
Blake <blakeedwincalkins@gmail.com>
2025-09-09 15:38:56 -0600

    add new classes and started funcitons

---
+++ game.py
@@ -0,0 +1,12 @@
+import random
+
+def rogue():
+    return random.randint(1, 4) + random.randint(1, 4) + random.randint(1, 4)
+def warrior():
+    return random.randint(1, 8) + 1
+
+def test_ten_thousand_times(which_class):
+    pass
+
+def main(): 
+    pass
