6fb10ab4f5c1a327d2cd9ded0d2d2bded82fc77d
Blake <blakeedwincalkins@gmail.com>
2025-09-09 22:26:04 -0600

    add strategist() and speedster()

--- game.py
+++ game.py
@@ -1,13 +1,25 @@
 import random
 
 def rogue():
-    return random.randint(1, 4) + random.randint(1, 4) + random.randint(1,4)
+    return random.randint(1, 4) + random.randint(1, 4)
 def warrior():
     return random.randint(1, 8) + 1
+def strategist():
+    return max(random.randint(1, 6), random.randint(1, 6), random.randint(1, 6))
 def sharpshooter():
-    return random.choice([2, 9])
+    return random.choice([2, 8])
 def dave_from_accounting():
     return 5
+def speedster():
+    sum = 0
+    roll = random.choice([1, 4])
+    sum += roll
+    if roll == 4:
+        while roll == 4:
+            roll = random.choice([1, 4])
+            sum += roll 
+    return sum
+
 
 def test_one_hundred_thousand_times(which_class):
     sum = 0
@@ -19,7 +31,9 @@ def main():
     # print(rogue())
     # print(warrior())
     # print(sharpshooter())
-    # print(test_one_hundred_thousand_times(sharpshooter))
+    print("strategist: ", test_one_hundred_thousand_times(strategist))
     return
 
+
+# print("total is: ", speedster())
 main()
