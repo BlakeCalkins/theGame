9dad2e034e07834c8bf1345ef230eb7070b26368
Blake <blakeedwincalkins@gmail.com>
2025-09-09 23:01:40 -0600

    add musketeer, bard, and num_turns

--- game.py
+++ game.py
@@ -1,16 +1,25 @@
 import random
 
-def rogue():
+def rogue(turn):
     return random.randint(1, 4) + random.randint(1, 4)
-def warrior():
+def warrior(turn):
     return random.randint(1, 8) + 1
-def strategist():
+def strategist(turn):
     return max(random.randint(1, 6), random.randint(1, 6), random.randint(1, 6))
-def sharpshooter():
+def musketeer(turn):
+    if turn % 2 == 0:
+        return random.randint(1, 20)
+    if turn % 2 == 1:
+        return 0
+def bard(turn):
+    a = random.randint(1, 20)
+    b = random.randint(1, 20)
+    return max(0, abs(a-b) - 2)
+def sharpshooter(turn):
     return random.choice([2, 8])
-def dave_from_accounting():
+def dave_from_accounting(turn):
     return 5
-def speedster():
+def speedster(turn):
     sum = 0
     roll = random.choice([1, 4])
     sum += roll
@@ -24,14 +33,40 @@ def speedster():
 def test_one_hundred_thousand_times(which_class):
     sum = 0
     for i in range(100000):
-        sum += which_class()
+        sum += which_class(1)
     return sum / 100000
 
+def test_one_hundred_thousand_times_turns(which_class):
+    sum = 0
+    for i in range(100000):
+        sum += num_turns(which_class)
+    return sum / 100000
+
+def num_turns(which_class):
+    turn = 1 
+    total = 0
+    while total < 20:
+        total += which_class(turn)
+        if total >= 20:
+            return turn
+        turn += 1
+
+
 def main(): 
-    # print(rogue())
-    # print(warrior())
-    # print(sharpshooter())
-    print("strategist: ", test_one_hundred_thousand_times(strategist))
+    # print("rogue: ", test_one_hundred_thousand_times(rogue))
+    # print("warrior: ", test_one_hundred_thousand_times(warrior))
+    # print("strategist: ", test_one_hundred_thousand_times(strategist))
+    print("bard: ", test_one_hundred_thousand_times(bard))
+    # print("sharpshooter: ", test_one_hundred_thousand_times(sharpshooter))
+    # print("dave_from_accounting: ", test_one_hundred_thousand_times(dave_from_accounting))
+    # print("musketeer: ", test_one_hundred_thousand_times_turns(musketeer))
+    # print("turns it took: ", num_turns(musketeer))
+    # print("turns it took: ", num_turns(musketeer))
+    # print("turns it took: ", num_turns(musketeer))
+    # print("turns it took: ", num_turns(musketeer))
+
+
+
     return
 
 
