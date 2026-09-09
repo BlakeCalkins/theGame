419a0ba7519e135a3d52332b75f296da9af3be21
Blake <blakeedwincalkins@gmail.com>
2026-09-08 23:26:17 -0600

    Patch 15

--- game.py
+++ game.py
@@ -161,7 +161,7 @@ class Irradiated(Archetype):
         self.name = name
     def calc_dmg(self, turn):
         if turn == 1:
-            return d4() + d4() + d4() + d4()
+            return d4() + d4() + d4() + d4() + d4()
         if turn == 2:
             return d4() + d4()
         else:
@@ -174,6 +174,7 @@ class Werewolf(Archetype):
         self.werewolf = False
     def calc_dmg(self, turn=None):
         if self.werewolf:
+            self.werewolf = False
             return d10()
         else:
             roll = d4()
@@ -187,7 +188,7 @@ class Needler(Archetype):
         self.name = name
     def calc_dmg(self, turn=None):
         dmg = 0
-        for _ in range(18):
+        for _ in range(17):
             if d6() == 1 or d6() == 2:
                 dmg += 1
         return dmg
@@ -348,7 +349,7 @@ def main():
     rad = Irradiated()
     archetypes = [rog, war, stg, msk, brd, ndl, frc, pal, acd, shs, dav, wer, rad, spd, wim]
     matchups = 0
-    with open("patch_14", "w") as f:
+    with open("patch_15", "w") as f:
         for i, type1 in enumerate(archetypes):
             for type2 in archetypes[i+1:]:
                 print(f"Testing {type1.name} vs {type2.name}", file=f)
@@ -396,7 +397,7 @@ def mirrors():
     wer1 = Werewolf("Werewolf A")
     wer2 = Werewolf("Werewolf B")
 
-    with open("patch_14", "a") as f:
+    with open("patch_15", "a") as f:
         print(f"Testing {rog1.name} vs {rog2.name}", file=f)
         print(f"Testing {rog1.name} vs {rog2.name}")
         test_one_hundred_thousand_games(rog1, rog2, output=f)
